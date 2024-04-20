6eb357b9d82ce45fa5d11b6f5ff0ccfa990738e2
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-04-20 23:40:29 +0530

    api axios connection is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [movies, setMovies] = useState();
+
+  const getMoviews = async () => {
+    try {
+      const response = await api.get('/api/v1/movies');
+      console.log(response.data);
+      setMovies(response.data);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getMoviews();
+  }, []);
+
   return (
     <h1>Hello World</h1>
   );
