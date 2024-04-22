8974f4447b9b5c8a1d7b95494ad8b408049af9f4
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-04-22 15:14:59 +0530

    Project is completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Layout, Home, Header, Trailer } from "./components/index";
+import { Layout, Home, Header, Trailer, Reviews } from "./components/index";
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMoviews = async () => {
     try {
@@ -15,6 +17,19 @@ function App() {
       setMovies(response.data);
     }
     catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getMovieData = async (movieId) => {
+    console.log("Movie ID : " + movieId);
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviewIds);
+      
+    } catch (error) {
       console.error(error);
     }
   }
@@ -30,6 +45,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />} />
           <Route path="/Trailer/:ytTrailerId" element={<Trailer />} />
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />} />
         </Route>
       </Routes>
     </div>
