8f365dd1ee1f70cd638b987120b5916d4753e9f4
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-04-22 11:43:05 +0530

    Header is done

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import { Button, Container, Nav, Navbar, NavLink } from "react-bootstrap"
+
+const Header = () => {
+    return (
+        <Navbar bg='dark' variant='dark' expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="/" style={{"color": "gold"}}>
+                    <FontAwesomeIcon icon={faVideoSlash} />
+                    <span style={{"marginLeft": "10px"}}>MovieDB</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id='navbarScroll'>
+                    <Nav className="me-auto my-2 my-lg-0" style={{maxHeight: "100px"}} navbarScroll>
+                        <NavLink className='nav-link' href="/" style={{"color": "white"}}>Home</NavLink>
+                        <NavLink className='nav-link' href="/watchList" style={{"color": "white"}}>Watch List</NavLink>
+                    </Nav>
+                    <Button variant='outline-info' className='me-2'>Login</Button>
+                    <Button variant='outline-info'>Register</Button>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
